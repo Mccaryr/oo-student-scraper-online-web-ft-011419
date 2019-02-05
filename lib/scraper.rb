a7de92617219599f1e735a532356260dab0e00e6
Robert McCary <robert.mccary@yahoo.com>
2019-02-05 14:31:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,7 @@ class Scraper
             url = student.attribute("href")
             students_hash[:twitter_url] = url if url.include?("twitter")
             students_hash[:linkedin_url] = url if url.include?("linkedin")
-            students_hash[:github_url] = url if url.include?("github")
+            # students_hash[:github_url] = url if url.include?("github")
             students_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
         end
             students_hash[:profile_quote] = html.css("div.profile-quote").text
